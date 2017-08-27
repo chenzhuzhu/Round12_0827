4a5ed758db695db80cfe07e578312a84deb9f444
陈昱竹 <826537618@qq.com>
2017-08-27 15:10:03 +0800

    reconstruct

--- main.js
+++ main.js
@@ -2,29 +2,61 @@
  * Created by cyz on 2017/8/27.
  */
 "use strict";
-function produceArr(){
-    let result=[]
-    for(let i=1;i<=26;i++){
-        result.push(i)
+function transArr(collection){
+    let result =[];
+    collection.forEach((item)=>{
+        let tempResult='';
+        item.forEach((ele)=>{
+            tempResult+=ele
+        })
+        result.push(parseInt(tempResult,10))
+    })
+    return result
+}
+
+function getMethod(collection){
+    let result = 0
+
+    collection.forEach((item)=>{
+        if((10<item)&&(item<=26)){
+            result+=2;
+        }else{
+            result+=1;
+        }
+    })
+
+    return result
+}
+
+
+function calculateMethod(num){
+    let tempArr=[]
+    let transNum=num.toString().split('');
+    for(let i =0;i<=transNum.length-2;i=i+2){
+        tempArr.push(transNum.slice(i,i+2))
+    }
+    let transedArr=transArr(tempArr)
+    let result = getMethod(transedArr)
+    if(transNum.length%2===1){
+        result+=1
     }
     return result
 }
 
 function main(num){
 
-    let arr =produceArr()
-    let result =0
-    if(num=='0'){
+    let result;
+    if(num===0){
         result=0;
-    }else if(num.length==2){
-        result=2
-    }else{
-        result
+    }else if((0<num)&&(num<=10)){
+        result =1;
+    }else {
+        result =calculateMethod(num)
     }
-
-
-
+    console.log('有'+result+'种编码方式')
 
 
 }
-main()
+let toAns=26
+console.log(toAns)
+main(toAns)
