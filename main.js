9522112e64a07e36b2bfa1d7cdf9cd4079750db9
陈昱竹 <826537618@qq.com>
2017-08-27 14:27:57 +0800

    finished

---
+++ main.js
@@ -0,0 +1,30 @@
+/**
+ * Created by cyz on 2017/8/27.
+ */
+"use strict";
+function produceArr(){
+    let result=[]
+    for(let i=1;i<=26;i++){
+        result.push(i)
+    }
+    return result
+}
+
+function main(num){
+
+    let arr =produceArr()
+    let result =0
+    if(num=='0'){
+        result=0;
+    }else if(num.length==2){
+        result=2
+    }else{
+        result
+    }
+
+
+
+
+
+}
+main()
